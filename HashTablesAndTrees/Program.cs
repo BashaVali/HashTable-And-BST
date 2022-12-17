776640e8bf94c8990dcf9302fea0eaa48a0d1614
BashaVali <bashavali222@gmail.com>
2022-12-17 07:59:43 +0530

    Binary Search Tree

--- HashTablesAndTrees/Program.cs
+++ HashTablesAndTrees/Program.cs
@@ -14,9 +14,8 @@ namespace HashTablesAndTrees
         {
 
             Console.WriteLine(" BINARY SEARCH TREE ");
-            Console.WriteLine("1.Add BST .");
-            Console.WriteLine("2.Add Emelents in BST .");
-            Console.WriteLine("Enter the option");
+            Console.WriteLine("1.Add BST \n2.Add Emelents in BST \n3.Search the element");
+            Console.WriteLine("Enter the option!!!");
             int num = Convert.ToInt32(Console.ReadLine());
             BST<int> binarySearchTree = new BST<int>(56);
             switch (num)
@@ -42,11 +41,27 @@ namespace HashTablesAndTrees
                     binarySearchTree.Display();
                     binarySearchTree.GetSize();
                     break;
+                case 3:
+                    binarySearchTree.Insert(30);
+                    binarySearchTree.Insert(70);
+                    binarySearchTree.Insert(22);
+                    binarySearchTree.Insert(40);
+                    binarySearchTree.Insert(11);
+                    binarySearchTree.Insert(3);
+                    binarySearchTree.Insert(16);
+                    binarySearchTree.Insert(60);
+                    binarySearchTree.Insert(95);
+                    binarySearchTree.Insert(65);
+                    binarySearchTree.Insert(63);
+                    binarySearchTree.Insert(67);
+                    binarySearchTree.Display();
+                    bool result = binarySearchTree.IfExists(63, binarySearchTree);
+                    Console.WriteLine(result);
+                    break;
                 default:
                     Console.WriteLine("Enter valid option!!!");
                     break;
             }
-            Console.ReadLine();
         }
     }
 }
